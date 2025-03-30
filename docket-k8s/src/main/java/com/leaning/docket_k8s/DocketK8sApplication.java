b14b3a2d3499fb3fcbad743a701774f4c752f13a
piyushmalav <piyushmalav85@gmail.com>
2025-03-31 01:35:39 +0530

    spring boot basic project with Dockerfile running on default tomacatt port 8080

---
+++ docket-k8s/src/main/java/com/leaning/docket_k8s/DocketK8sApplication.java
@@ -0,0 +1,13 @@
+package com.leaning.docket_k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocketK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocketK8sApplication.class, args);
+	}
+
+}
---
+++ docket-k8s/src/test/java/com/leaning/docket_k8s/DocketK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaning.docket_k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocketK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
